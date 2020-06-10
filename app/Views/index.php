fc79a3f925890f70e3daaf5a7d4024a075c88b65
jesimielefriamdias <jesimiel.dias@gmail.com>
2020-06-10 19:10:37 -0300

    Segundo commit

--- app/Views/index.php
+++ app/Views/index.php
@@ -92,7 +92,7 @@
     
     <input id = "base_url" type = "hidden" value = "<?php echo base_url(); ?>";>
     
-    <!-- Optional JavaScript -->
+    <!-- Optional JavaScript --aaaaa>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
