b65ddfd2f8f68187d5ee6c56d37226ba3d217924
Jesimiel Efraim Dias <jesimiel.dias@gmail.com>
2020-06-11 19:41:52 -0300

    cadastro e login

--- app/Controllers/Home_controller.php
+++ app/Controllers/Home_controller.php
@@ -4,16 +4,25 @@ use CodeIgniter\Controller;
 
 class Home_controller extends Controller
 {
-	public function index()
+	public function __construct()
 	{
 		session_start();
 
 		if(isset($_SESSION["grau_acesso"]) && $_SESSION["grau_acesso"] != "I"){
 			return view('home.php');
+		} else {
+
 		}
+	
+	}
+	public function index()
+	{
+		session_start();
+
+		return view('home.php');
+		
 		
 	}
-
 	//--------------------------------------------------------------------
 
 }
